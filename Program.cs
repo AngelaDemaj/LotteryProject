d2576b7c91d86d1e97d4846370730bb8474b224e
AngelaDemaj <aggelademaj@hotmail.com>
2021-01-09 15:39:08 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LotteryProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
